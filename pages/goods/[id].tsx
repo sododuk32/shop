5f98e7d1d8436aa8d4fe4aef4b79d6e50fba84ce
sododuk32 <37325163+sododuk32@users.noreply.github.com>
2022-10-24 16:17:21 +0900

    11

---
+++ pages/goods/[id].tsx
@@ -0,0 +1,11 @@
+import styles from './[id].module.css';
+function Goods() {
+  return (
+    <div>
+      this is goods number page
+      <div className={styles.divdiv2}>asdfsadf</div>
+    </div>
+  );
+}
+
+export default Goods;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,26 @@
+/* eslint-disable react/jsx-no-duplicate-props */
 /* eslint-disable no-unused-vars */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import type { NextPage } from 'next';
 import styles from './index.module.css';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
-import Cards from '../components/Cards';
+import CardRows from 'components/CardRows';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+
 const Home: NextPage = () => {
+  const dataArray: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
   return (
     <div>
       <Header />
-      <div id="bodyContainer" className={styles.bodyContainer}>
-        <Cards />
-      </div>
+      <Box sx={{ margin: 5, display: 'flex', justifyContent: 'center' }}>
+        <Grid container sm={'auto'} md={10} spacing={{ md: 3 }} direction="row" justifyContent="center" alignItems="center">
+          {dataArray.map((e) => {
+            return CardRows(e);
+          })}
+        </Grid>
+      </Box>
       <Footer />
     </div>
   );
