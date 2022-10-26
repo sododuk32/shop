8dff0988fc3d508217794756b413f6cf5b3f3acc
sododuk32 <37325163+sododuk32@users.noreply.github.com>
2022-10-26 18:41:20 +0900

    asdf

--- components/Carouselnomal.tsx
+++ components/Carouselnomal.tsx
@@ -1,25 +1,74 @@
 import React from 'react';
 import styles from './Carouselnomal.module.css';
 import Image from 'next/image';
-import { Carousel } from 'react-responsive-carousel';
-
+import { useState } from 'react';
 function Carouselnomal() {
+  const [slide, setslide] = useState(1);
+  function prevbtn() {
+    if (slide === 0) {
+      console.log('0일때 시행중');
+      setslide(3);
+      const nowSlide = document.getElementById('box' + slide)!;
+      nowSlide.style.zIndex = '2';
+      console.log('prev끝날때' + slide);
+      return setslide(slide - 1);
+    }
+    if (slide > 1) {
+      console.log('1보다클때 시행중');
+
+      const nowSlide = document.getElementById('box' + slide)!;
+      nowSlide.style.zIndex = '2';
+      console.log('prev끝날때' + slide);
+      return setslide(slide - 1);
+    }
+    console.log('prev끝날때' + slide);
+  }
+
+  function nextbtn() {
+    if (slide === 3) {
+      console.log('4일때 시행중');
+      setslide(1);
+      let nowSlide = document.getElementById('box' + slide)!;
+      nowSlide.style.zIndex = '2';
+      nowSlide = document.getElementById('box' + 3)!;
+      nowSlide.style.zIndex = '1';
+      console.log('prev끝날때' + slide);
+      return setslide(slide + 1);
+    }
+    if (slide < 3) {
+      console.log('3보다작을때 시행중');
+      let nowSlide = document.getElementById('box' + slide)!;
+      nowSlide.style.zIndex = '2';
+      nowSlide = document.getElementById('box' + slide - 1)!;
+      nowSlide.style.zIndex = '1';
+      console.log('prev끝날때' + slide);
+      return setslide(slide + 1);
+    }
+    console.log('next끝날때' + slide);
+  }
+
   return (
     <div>
-      <Carousel>
-        <div className={styles.carouselContainer}>
-          <Image layout="responsive" width={1200} height={450} quality={100} className={styles.carouselImg} src={'/pcrplastic.jpg'} />
+      <div className={styles.carouselBox}>
+        <button className={styles.prevBtn} onClick={prevbtn}>
+          PREV
+        </button>
+        <div id="box1" className={styles.carouselContainer}>
+          <Image layout="responsive" width={1200} height={450} quality={100} className={styles.carouselImg1} src={'/pcrplastic.jpg'} />
           <span>1</span>
         </div>
-        <div className={styles.carouselContainer}>
-          <Image layout="responsive" width={1200} height={450} quality={100} className={styles.carouselImg} src={'/sircuit.jpg'} />
+        <div id="box2" className={styles.carouselContainer}>
+          <Image layout="responsive" width={1200} height={450} quality={100} className={styles.carouselImg2} src={'/sircuit.jpg'} />
           <span>2</span>
         </div>
-        <div className={styles.carouselContainer}>
-          <Image layout="responsive" width={1200} height={450} quality={100} className={styles.carouselImg} src={'/logicares.jpg'} />
+        <div id="box3" className={styles.carouselContainer}>
+          <Image layout="responsive" width={1200} height={450} quality={100} className={styles.carouselImg3} src={'/logicares.jpg'} />
           <span>3</span>
         </div>
-      </Carousel>
+        <button className={styles.nextBtn} onClick={nextbtn}>
+          NEXT
+        </button>
+      </div>
     </div>
   );
 }
