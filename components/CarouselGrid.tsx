f7615fdead86b63ae5a7c902b9a84449a6612921
sododuk32 <37325163+sododuk32@users.noreply.github.com>
2022-10-29 19:49:37 +0900

    20

--- components/CarouselGrid.tsx
+++ components/CarouselGrid.tsx
@@ -1,20 +1,56 @@
 import React from 'react';
 import styles from './CarouselGrid.module.css';
+import Image from 'next/image';
 function CarouselGrid() {
   //custom attirbute가 있는대 이걸 사용해서
   //css를 변경 가능해 보인다.
+  // 이미지 쓸꺼면 image상위태그에 반드시 position relative
   return (
     <div>
-      <div className={styles.boxContainer}>
-        <div className={styles.gridContainer} tabIndex={1} data-cmp-hook-carousel={'1'}>
-          <div className={styles.griditem1}>1</div>
-          <div className={styles.griditem1}>2</div>
-          <div className={styles.griditem1}>3</div>
-          <div className={styles.griditem1}>4</div>
+      <section className={styles.gridsContainer}>
+        <div className={styles.gridbox}>
+          <div className={styles.keyboardwhite}>
+            <Image src={'/keyboardwhite.jpg'} layout="fill" objectFit="scale-down" />
+          </div>
+          <div className={styles.headset}>
+            <Image src={'/headset.jpg'} layout="fill" objectFit="scale-down" />
+          </div>
+          <div className={styles.laptop}>
+            <Image src={'/laptop.jpg'} layout="fill" objectFit="scale-down" />
+          </div>
+          <div className={styles.webcam}>
+            <Image src={'/webcam.jpg'} layout="fill" objectFit="scale-down" />
+          </div>
         </div>
-        <div className={styles.gridContainer}></div>
-        <div className={styles.gridContainer}></div>
-      </div>
+        <div className={styles.gridbox}>
+          <div className={styles.keyboardwhite}>
+            <Image src={'/keyboardwhite.jpg'} layout="fill" objectFit="scale-down" />
+          </div>
+          <div className={styles.headset}>
+            <Image src={'/headset.jpg'} layout="fill" objectFit="scale-down" />
+          </div>
+          <div className={styles.laptop}>
+            <Image src={'/laptop.jpg'} layout="fill" objectFit="scale-down" />
+          </div>
+          <div className={styles.webcam}>
+            <Image src={'/webcam.jpg'} layout="fill" objectFit="scale-down" />
+          </div>
+        </div>
+        <div className={styles.gridbox}>
+          <div className={styles.keyboardwhite}>
+            <Image src={'/keyboardwhite.jpg'} layout="fill" objectFit="scale-down" />
+          </div>
+          <div className={styles.headset}>
+            <Image src={'/headset.jpg'} layout="fill" objectFit="scale-down" />
+          </div>
+          <div className={styles.laptop}>
+            <Image src={'/laptop.jpg'} layout="fill" objectFit="scale-down" />
+          </div>
+          <div className={styles.webcam}>
+            <Image src={'/webcam.jpg'} layout="fill" objectFit="scale-down" />
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
