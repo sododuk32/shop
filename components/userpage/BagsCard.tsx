574561d267559b754c9395bb44ed69a4feb58078
sododuk32 <37325163+sododuk32@users.noreply.github.com>
2023-01-17 16:50:29 +0900

    fix: 장바구니 조작 추가

--- components/userpage/BagsCard.tsx
+++ components/userpage/BagsCard.tsx
@@ -1,45 +1,72 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import React, { useRef, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import styles from './BagsCard.module.css';
 import { productInfo } from 'lib/redux/interface';
-import { UserOption } from 'lib/redux/interface';
-import Form from 'react-bootstrap/Form';
 import { cartTest } from 'lib/redux/reducers/getUserSlice';
 import { store } from 'store';
 import { useSelector } from 'react-redux';
+import { Button } from 'react-bootstrap';
 
+// eslint-disable-next-line @typescript-eslint/ban-types
 function BagsCard(item: productInfo) {
-  const amoutArr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-  const [thisamount, setamount] = useState(0);
-  const mycart = useSelector(cartTest);
-  const changeAmount = (e: any) => {
-    return setamount(e.target.value);
-  };
-  // console.log(e.target.value);
-  // if (thisamount > 0) {
-  //   sendPrice = thisamount * item.price;
-  //   return store.dispatch(pricing(sendPrice));
-  // } else {
-  //   sendPrice = item.amount * item.price;
-  //   return store.dispatch(pricing(sendPrice));
-  // }
+  const [thisAmount, setAmount] = useState(0);
+  const myprice = useRef<HTMLSpanElement>(null);
+  const fixedPrice = item.amount * item.price;
+  const changedPrice = thisAmount * item.price;
+  let priceValue: number;
+  useEffect(() => {
+    if (myprice.current != null) {
+      priceValue = Number(myprice.current.innerHTML);
+    }
+  }, [thisAmount]);
+
+  function changeAmountPlus(e: any) {
+    let temp = priceValue + 1;
+    if (temp > 101) {
+      temp = 100;
+    }
+    return setAmount(temp);
+  }
+  function changeAmountMinus(e: any) {
+    let temp = priceValue - 1;
+    if (temp === 0) {
+      temp = 1;
+    }
+    return setAmount(temp);
+  }
+
   return (
     <div>
-      <section className={styles.BagsCardBody}>
-        <div className={styles.cards}>상품명:{item.productId}</div>
-        <div className={styles.amount}>
-          <div className={styles.cards}>수량:{thisamount > 0 ? thisamount : item.amount}</div>
-          <input type="text" id="amount1" className={styles.amountOption} onChange={changeAmount} />
+      <div className={styles.cards}>상품명:{item.productId}</div>
+      <div className={styles.amount}>
+        <span>수량:</span>
+        <div className={styles.amountchng}>
+          <Button className={styles.changeBtn} onClick={changeAmountMinus} variant="secondary" size="sm">
+            -
+          </Button>
+          <div>
+            <span ref={myprice} className={styles.cards}>
+              {thisAmount > 0 ? thisAmount : item.amount}
+            </span>
+          </div>
+          <Button className={styles.changeBtn} onClick={changeAmountPlus} variant="primary" size="sm">
+            +
+          </Button>
         </div>
-        <div className={styles.cardOptions}>
-          <span>옵션</span>
-          <span>색깔:{item.option.ProductColor}</span>
-          <span>사이즈:{item.option.ProductSize}</span>
-          <span>케이블종류:{item.option.ProductWireless}</span>
-          <span>{item.option.ProductHands ? `손: ${item.option.ProductHands}` : null}</span>
-        </div>
+      </div>
+      <div className={styles.cardOptions}>
+        <span>옵션</span>
+        <span>색깔:{item.option.ProductColor}</span>
+        <span>사이즈:{item.option.ProductSize}</span>
+        <span>케이블종류:{item.option.ProductWireless}</span>
+        <span>{item.option.ProductHands ? `손: ${item.option.ProductHands}` : null}</span>
+      </div>
+
+      <section className={styles.pricing}>
+        <span>가격:</span>
+        <span id="valueOfprice">{thisAmount > 0 ? changedPrice : fixedPrice}</span>
       </section>
-      <section className="pricing">가격:{thisamount > 0 ? thisamount * item.price : item.amount * item.price}</section>
     </div>
   );
 }
--- pages/users/bags.tsx
+++ pages/users/bags.tsx
@@ -1,4 +1,5 @@
-import React, { useRef } from 'react';
+/* eslint-disable prefer-const */
+import React, { useEffect, useState } from 'react';
 import styles from './bags.module.css';
 import Header from 'components/commons/Headers/Header';
 import SetLanguage from 'components/commons/Headers/SetLanguage';
@@ -6,11 +7,19 @@ import BagsCard from 'components/userpage/BagsCard';
 import { productInfo } from 'lib/redux/interface';
 import { cartTest } from 'lib/redux/reducers/getUserSlice';
 import { useSelector } from 'react-redux';
+//      price = myBags.current.childNodes[0].lastChild.childNodes[3].childNodes[1];
 
 function bags() {
   const mycart = useSelector(cartTest);
-  const val = document.querySelectorAll('li');
-  console.log(val);
+  const [totalPrice, setprice] = useState(0);
+  // const [productsPrice, setProductprice] = useState<number[]>([]);
+
+  useEffect(() => {
+    let myBags = document.querySelectorAll('#valueOfprice');
+    let temPice = 0;
+    myBags.forEach((element) => (temPice += Number(element.innerHTML)));
+    setprice(temPice);
+  }, [totalPrice]);
   return (
     <div>
       <header className={styles.header}>
@@ -27,7 +36,7 @@ function bags() {
             ))}
           </div>
           <div className={styles.billPannel}>
-            <span>총 가격: {}</span>
+            <span>총 가격: {totalPrice}</span>
             <button className={styles.buyingBtn}>구매하기</button>
           </div>
         </div>
